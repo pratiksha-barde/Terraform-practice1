8a9f2757a5f43c8b38f5f0d09d08aebfc68970f3
pratii <pratikshabarde2002@gmail.com>
2025-05-04 08:10:08 +0530

    Day 4-statefile-remote-config-s3/

---
+++ Day 4-statefile-remote-config-s3/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "dev" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+resource "aws_subnet" "test" {
+    cidr_block = "10.0.0.0/24"
+  vpc_id =aws_vpc.dev.id
+}
+
+
+resource "aws_subnet" "dev" {
+    cidr_block = "10.0.1.0/24"
+  vpc_id =aws_vpc.dev.id
+}
